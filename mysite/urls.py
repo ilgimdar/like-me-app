c332805d2c3a9ee014164e899cf2c888b0283a10
Ildar Gimadeev <43295998+ilgimdar@users.noreply.github.com>
2022-03-24 11:24:55 +0300

    Деплой 6

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('datings.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
 ]
